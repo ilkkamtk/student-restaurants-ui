d2e5af06b503419b7732006ed4ea74e285b30a66
Ilkka Kylmäniemi <ilkka.kylmaniemi@metropolia.fi>
2023-06-09 19:25:35 +0300

    env testi

--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,4 +1,4 @@
-const apiURL = import.meta.env.VITE_MAPBOX_TOKEN as string;
+const apiURL = import.meta.env.VITE_API_URL as string;
 const mapBoxToken = import.meta.env.VITE_MAPBOX_TOKEN as string;
 const uploadUrl = import.meta.env.VITE_UPLOAD_URL as string;
 const googleApiKey = import.meta.env.VITE_GOOGLE_API_KEY as string;
